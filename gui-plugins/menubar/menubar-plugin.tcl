a7cc59bfc37991247bfc812bf4a07b346170d970
Yvan Volochine <yvan.volochine@gmail.com>
2011-04-05 17:39:26 +0200

    check for OS as it's now linux only

--- gui-plugins/menubar/menubar-plugin.tcl
+++ gui-plugins/menubar/menubar-plugin.tcl
@@ -1,14 +1,11 @@
 # META NAME auto-complete plugin
 # META DESCRIPTION Does auto-completion for objects
 # META AUTHOR <Yvan Volochine> yvan.pd@mail.com
-# META VERSION 0.1
+# META VERSION 0.11
 
 package require Tcl 8.4
 package require pdwindow 0.1
 
-# GLOBALS
-set ::pdwindow_menubar ".menubar"
-set ::patch_menubar   ".menubar"
 
 proc recreate_menubar {} {
     # FIXME delete menubar and re-create it
@@ -24,11 +21,12 @@ proc recreate_menubar {} {
         .menubar add cascade -label [_ [string totitle $mymenu]] \
             -underline $underlined -menu .menubar.$mymenu
     }
-    if {$::windowingsystem eq "aqua"} {create_apple_menu .menubar}
-    if {$::windowingsystem eq "win32"} {create_system_menu .menubar}
     . configure -menu .menubar
 }
 
-recreate_menubar
-
-pdtk_post "loaded: menubar-plugin 0.1\n"
+if {$::windowingsystem eq "x11"} {
+    recreate_menubar
+    pdtk_post "loaded: menubar-plugin 0.1\n"
+} {
+    pdtk_post "WARNING:\nmenubar-plugin is not osx or win32 compatible\n"
+}
