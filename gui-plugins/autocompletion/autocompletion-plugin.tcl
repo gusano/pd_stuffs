55540840b91fd3399fb3002bfc82b3d5c0527f50
Yvan Volochine <yvan.volochine@gmail.com>
2011-05-06 01:29:01 +0200

    new version, major rewrite, still alpha

--- gui-plugins/autocompletion/autocompletion-plugin.tcl
+++ gui-plugins/autocompletion/autocompletion-plugin.tcl
@@ -1,88 +1,115 @@
 # META NAME auto-completion plugin
 # META DESCRIPTION enables auto-completion for objects
 # META AUTHOR <Yvan Volochine> yvan.volochine@gmail.com
-# META VERSION 0.33
+# META VERSION 0.40
 
-package require Tcl 8.4
+# TODO
+# - add user arguments (tabread $1 ...)
+# - wrap around completions
 
+# BUGS FIXME
+# "list ..." is buggy
+# * / etc should NOT trigger popup
+
+package require Tcl 8.5
 namespace eval ::completion:: {}
 
 ###########################################################
-# overwritten procedures
-rename ::pd_bindings::sendkey ::pd_bindings::sendkey_old
+# overwritten
 rename pdtk_text_set pdtk_text_set_old
 rename pdtk_text_editing pdtk_text_editing_old
 rename ::dialog_font::ok ::dialog_font::ok_old
 
-###########################################################
+############################################################
 # GLOBALS
 
 # this is where you can put extra objects/abstractions that you want to
 # work with auto-completion. BEWARE, you should have *only one* object/abstraction
-# name per line! if your patch/abstraction name has spaces in its name, you *have to*
-# put the name into quotes: "my cool abstraction"
+# name per line!
 set ::user_objects_list "~/pd/list_of_my_objects.txt"
 #set ::user_objects_list ""
 
+
+############################################################
+# private
+
+set ::external_filetype ""
 set ::new_object false
+set ::toplevel ""
 set ::current_canvas ""
 set ::current_tag ""
 set ::lock_motion false
 set ::editx 0
 set ::edity 0
-set ::font_size 10
+set ::font_size 10 ;# FIXME ???
 set ::current_text ""
-set ::first_text ""
 set ::erase_text ""
 set ::i 0
-set ::cycle false
 set ::completions {}
-# all pd internals (hopefully)
-set ::all_externals {random loadbang namecanvas serial cputime realtime \
-canvas declare template curve plot drawnumber vradio declare mtof ftom \
-rmstodb powtodb dbtopow dbtorms max~ min~ delwrite~ delread~ vd~ inlet~ \
-outlet~ block samplerate~ inlet midiin sysexin notein ctlin pgmin bendin \
-touchin polytouchin midiclkin midirealtimein midiout noteout ctlout pgmout \
-bendout touchout polytouchout makenote stripnote poly bag "list append" \
-"list prepend" "list split" "list trim" "list length" hradio print text \
-cos~ osc~ vcf~ noise~ hslider hip~ lop~ bp~ biquad~ samphold~ rpole~ rzero~ \
-rzero_rev~ cpole~ czero~ czero_rev~ dac~ adc~ sig~ line~ vline~ snapshot~ \
-vsnapshot~ env~ threshold~ bang~ fft~ ifft~ rfft~ rifft~ framp~ qlist \
-textfile openpanel savepanel key keyup keyname int float symbol bang send \
-receive sel route pack unpack trigger spigot moses until makefilename swap \
-change value bng pow max min mod div sin cos tan atan atan2 sqrt log exp \
-abs wrap clip toggle soundfiler readsf writesf~ tabwrite~ tabplay~ tabread~ \
-tabread4~ tabosc4~ tabsend tabreceive tabread tabread4 tabwrite send~ \
-receive~ catch~ throw~ get set getsize setsize append sublist netsend \
-netreceive nbx vslider clip~ rsqrt~ sqrt~ wrap~ mtof~ ftom~ dbtorms~ \
-rmstodb~ dbtopow~ powtodb~ pow~ exp~ log~ abs~ text vu delay metro line \
-timer pipe list phasor~}
+set ::canvas_bound 0 ;# FIXME remove
 
+# all pd internals (hopefully)
+set ::all_externals {abs abs~ adc~ append atan atan2 bag bang bang~ bendin \
+bendout biquad~ block bng bp~ canvas catch~ change clip clip~ cos cos~ cpole~ \
+cputime ctlin ctlout curve czero_rev~ czero~ dac~ dbtopow dbtopow~ dbtorms \
+dbtorms~ declare delay delread~ delwrite~ div drawnumber env~ exp exp~ fft~ \
+float framp~ ftom ftom~ get getsize hip~ hradio hslider ifft~ inlet inlet~ int \
+key keyname keyup line line~ list {list append} {list length} {list prepend} \
+{list split} {list trim} loadbang log log~ lop~ makefilename makenote max max~ \
+metro midiclkin midiin midiout midirealtimein min min~ mod moses mtof mtof~ \
+namecanvas nbx netreceive netsend noise~ notein noteout openpanel osc~ outlet~ \
+pack pgmin pgmout phasor~ pipe plot poly polytouchin polytouchout pow powtodb \
+powtodb~ pow~ print qlist random readsf realtime receive receive~ rfft~ rifft~ \
+rmstodb rmstodb~ route rpole~ rsqrt~ rzero_rev~ rzero~ samphold~ samplerate~ \
+savepanel sel send send~ serial set setsize sig~ sin snapshot~ soundfiler spigot \
+sqrt sqrt~ stripnote struct sublist swap switch~ symbol sysexin tabosc4~ \
+tabplay~ tabread tabread4 tabread4~ tabread~ tabwrite tabwrite~ tan template \
+textfile threshold~ throw~ timer toggle touchin touchout trigger unpack until \
+value vcf~ vd~ vline~ vradio vslider vsnapshot~ vu wrap wrap~ writesf~}
 
 
 proc ::completion::init {} {
-    # bind Tab for autocompletion
-    bind all <Tab> {::completion::trigger}
-    ::completion::list_user_externals
-    ::completion::list_user_objects $::user_objects_list
-    # sort objects list for a quicker search later
+    switch -- $::windowingsystem {
+        "aqua"  { set ::external_filetype *.pd_darwin }
+        "win32" { set ::external_filetype *.dll }
+        "x11"   { set ::external_filetype *.pd_linux }
+    }
+    bind all <Tab> {+::completion::trigger}
+    ::completion::add_user_externals
+    ::completion::add_user_objects $::user_objects_list
     set ::all_externals [lsort $::all_externals]
 }
 
 proc ::completion::trigger {} {
     if {$::new_object} {
-        # remove whitespaces
-        set ::current_text [string map {" " ""} $::current_text]
-        ::completion::find_external
+        set length [llength $::completions]
+        if {![winfo exists .pop] || $length == 1} {
+            ::completion::find_completions
+        } {
+            if {![::completion::try_common_prefix]} {
+                ::completion::increment
+            }
+        }
     }
 }
 
-proc ::completion::list_user_externals {} {
+proc ::completion::increment {} {
+    focus .pop.f.lb
+    set selected [.pop.f.lb curselection]
+    set length [llength $::completions]
+    set updated [expr {($selected + 1) % [llength $::completions]}]
+    .pop.f.lb selection clear 0 end
+    .pop.f.lb selection set $updated
+    # FIXME and wrap
+    .pop.f.lb yview scroll 1 units
+}
+
+proc ::completion::add_user_externals {} {
     foreach pathdir [concat $::sys_searchpath $::sys_staticpath] {
         set dir [file normalize $pathdir]
         if { ! [file isdirectory $dir]} {continue}
         foreach filename [glob -directory $dir -nocomplain -types {f} -- \
-                              *.pd_linux] {
+                              $::external_filetype] {
             set basename [file tail $filename]
             set name [file rootname $basename]
             lappend ::all_externals $name
@@ -90,7 +117,7 @@ proc ::completion::list_user_externals {} {
     }
 }
 
-proc ::completion::list_user_objects {afile} {
+proc ::completion::add_user_objects {afile} {
     set filename [file join $afile]
     if {
         $afile ne ""
@@ -107,80 +134,129 @@ proc ::completion::list_user_objects {afile} {
     }
 }
 
-proc ::completion::find_external {} {
+proc ::completion::find_completions {} {
+    puts "finding for $::current_text"
     set length [llength $::completions]
-    set text $::current_text
-
-    if {$text ne "" && $::cycle == false} {
-        set ::erase_text $text
-        set ::completions [lsearch -all -inline -glob $::all_externals $text*]
-        # to retrieve typed text after cycling through all possible completions
-        set ::first_text $::current_text
-        set ::cycle true
+    if {$::current_text ne ""} {
+        ::completion::update
         set length [llength $::completions]
-        set ::i 0
-        set trigger_popup 1
-    } {
-        # popup is already there
-        set trigger_popup 0
     }
-
     set new_text [lindex $::completions $::i]
     if {$length > 0} {
         if {$length == 1} {
-            ::completion::write_text $new_text
+            ::completion::replace_text $new_text
+            catch { destroy .pop }
         } {
-            ::completion::popup $::i $trigger_popup
+            if {![winfo exists .pop]} { ::completion::popup_draw }
+            ::completion::try_common_prefix
         }
-        set ::i [expr ($::i + 1) % $length]
+    } { catch { destroy .pop } }
+}
+
+proc ::completion::update {} {
+    set ::erase_text $::current_text
+    set text $::current_text
+    # prevent wildcards
+    set text [string map {"*" "\\*"} $text]
+    set text [string map {"-" "\\-"} $text]
+    set ::completions [lsearch -all -inline -glob $::all_externals $text*]
+    if {[llength $::completions] == 0} { catch { destroy .pop } }
+    set ::i 0
+}
+
+proc ::completion::choose_selected {} {
+    set selected [.pop.f.lb curselection]
+    catch { destroy .pop}
+    focus -force $::toplevel
+    ::completion::replace_text [lindex $::completions $selected]
+    set ::current_text [lindex $::completions $selected]
+    set ::completions {}
+}
+
+# keys with listbox focus
+proc ::completion::lb_keys {key} {
+    if {[regexp {^[a-zA-Z0-9~/\._\+]{1}$} $key]} {
+        ::completion::insert_key $key; return
+    }
+    switch -- $key {
+        "space"     { ::completion::insert_key " " }
+        "Return"    { ::completion::choose_selected }
+        "BackSpace" { ::completion::chop 0 }
+    }
+#        "Tab"       { focus -force $::current_canvas }
+}
+
+# keys from textbox
+proc ::completion::text_keys {key} {
+    switch -- $key {
+        "plus"   { set key "+" }
+        "Escape" { catch { destroy .pop } }
+    }
+    if {[regexp {^[a-zA-Z0-9~/\._\+\-\*]{1}$} $key]} {
+        append ::current_text $key
+        ::completion::update
+    } elseif {$key eq "space"} {
+        append ::current_text " "
+        ::completion::update
+    } elseif {$key eq "BackSpace"} {
+        ::completion::chop 1
+        ::completion::update
+    } elseif {$key eq "Return"} {
+        if {[winfo exists .pop]} {
+            ::completion::choose_selected
+        } {
+            ::completion::text_unedit
+        }
     }
 }
 
-
-proc ::completion::popup {i {trigger 0}} {
-    set menuheight 32
-    if {$::windowingsystem ne "aqua"} { incr menuheight 24 }
-    if {$trigger} {
-        set mytoplevel [winfo toplevel $::current_canvas]
-        set geom [wm geometry $mytoplevel]
-        regexp -- {([0-9]+)x([0-9]+)\+([0-9]+)\+([0-9]+)} $geom -> \
-              width height decoLeft decoTop
-        set left [expr $decoLeft + $::editx]
-        set top [expr $decoTop + $::edity + $menuheight]
-        # popup menu
-        catch { destroy .completion_popup }
-        menu .completion_popup -tearoff 0
-        foreach name $::completions {
-            .completion_popup add command -label $name -command \
-                "::completion::write_text $name" -activebackground \
-                "#222222" -activeforeground "#DDDDDD" -background \
-                "#DDDDDD" -foreground "#222222"
-        }
-        tk_popup .completion_popup $left $top
-        .completion_popup entryconfigure 0 -state active
-    } {
-        .completion_popup entryconfigure $i -state active
+proc ::completion::chop {{fromtext 0}} {
+    if {!$fromtext} {
+        pdsend "$::toplevel key 1 8 0" ;# BackSpace
+        pdsend "$::toplevel key 0 8 0"
+    }
+    set ::current_text [string replace $::current_text end end]
+    ::completion::update
+    if {[winfo exists .pop]} {
+        .pop.f.lb selection clear 0 end
+        .pop.f.lb selection set 0
     }
 }
 
-# simulate backspace keys
-proc ::completion::erase_previoustext {} {
-    set mytoplevel [winfo toplevel $::current_canvas]
+proc ::completion::insert_key {key} {
+    scan $key %c keynum
+    pdsend "pd key 1 $keynum 0"
+    append ::current_text $key
+    ::completion::update
+    focus -force $::current_canvas
+    pdtk_text_editing $::toplevel $::current_tag 1
+}
+
+proc ::completion::popup_update_selection {inc} {
+    set newsel [expr {[.pop.lb curselection] + $inc}]
+    set newsel [expr {[expr {$newsel<0}]?0:$newsel}]
+    .pop.lb selection clear [.pop.lb curselection]
+    .pop.lb selection set $newsel
+    # boring hack: keep cursor at the end of textbox
+    $::current_canvas icursor $::current_tag 80
+}
+
+proc ::completion::erase_textbox {} {
+    # simulate backspace keys
     set i [string length $::erase_text]
-    while {$i > 0} {
-        pdsend "$mytoplevel key 1 8 0"
-        pdsend "$mytoplevel key 0 8 0"
+    while {--$i > 0} {
+        pdsend "$::toplevel key 1 8 0"
+        pdsend "$::toplevel key 0 8 0"
         incr i -1
     }
 }
 
-# write text into the object box
-proc ::completion::write_text {args} {
-    ::completion::erase_previoustext
+# replace text from object box
+proc ::completion::replace_text {args} {
     set text ""
+    ::completion::erase_textbox
     # in case of spaces
     foreach arg $args { set text [concat $text $arg] }
-    # write letters (!)
     for {set i 0} {$i < [string length $text]} {incr i 1} {
         set cha [string index $text $i]
         scan $cha %c keynum
@@ -190,63 +266,119 @@ proc ::completion::write_text {args} {
     set ::erase_text $text
 }
 
+proc ::completion::text_unedit {} {
+    set x [expr {$::editx - 2}]
+    set y [expr {$::edity - 2}]
+    ::completion::mouse $x $y
+    set x [expr {$::editx + 2}]
+    ::completion::mouseup $x $y
+    pdtk_text_editing $::toplevel $::current_tag 0
+    set ::new_object 0
+    # bouh, gui has no way to know what is selected..
+    #bind $::current_canvas <Return> { ::completion::text_edit }
+}
+
+# FIXME not used
+proc ::completion::text_edit {} {
+    set x [expr {$::editx + 4}]
+    set y [expr {$::edity - 4}]
+    # dble-click inside the object (maybe)
+    ::completion::mouse $x $y
+    ::completion::mouseup $x $y
+    ::completion::mouse $x $y
+    ::completion::mouseup $x $y
+    pdtk_text_editing $::toplevel $::current_tag 1
+    set old $::current_text
+    pdtk_text_set $::current_canvas $::current_tag {}
+    ::completion::replace_text $old
+}
+
+proc ::completion::popup_draw {} {
+    set menuheight 32
+    if {$::windowingsystem ne "aqua"} { incr menuheight 24 }
+    set geom [wm geometry $::toplevel]
+    regexp -- {([0-9]+)x([0-9]+)\+([0-9]+)\+([0-9]+)} $geom -> \
+        width height decoLeft decoTop
+    set left [expr {$decoLeft + $::editx}]
+    set top [expr {$decoTop + $::edity + $menuheight}]
+
+    catch { destroy .pop }
+    toplevel .pop
+    wm overrideredirect .pop 1
+    wm geometry .pop +$left+$top
+    frame .pop.f
+
+    pack configure .pop.f
+    .pop.f configure -relief solid -borderwidth 1 -background white
+
+    listbox .pop.f.lb -selectmode browse -height 7 -listvariable \
+        ::completions -activestyle none -highlightcolor white \
+        -selectbackground #418bd4 -selectforeground white \
+        -width 24 -yscrollcommand [list .pop.f.sb set] -takefocus 0
+    pack .pop.f.lb -side left -expand 1 -fill both
+    .pop.f.lb configure -font [list "DejaVu Sans Mono" $::font_size] \
+        -relief flat
+    .pop.f.lb selection set 0 0
+    pack .pop.f.lb [scrollbar ".pop.f.sb" -command [list .pop.f.lb yview]] \
+        -side left -fill y -anchor w
+
+    bind .pop.f.lb <Escape> {after idle {destroy .pop}}
+    bind .pop.f.lb <KeyPress> {::completion::lb_keys %K}
+    bind .pop.f.lb <ButtonRelease> {after idle {::completion::choose_selected}}
+    focus .pop.f.lb
+}
+
+proc ::completion::mouse {x y} {
+    pdsend "$::toplevel mouse [$::current_canvas canvasx $x] [$::current_canvas canvasy $y] 1 0"
+}
+
+proc ::completion::mouseup {x y} {
+    pdsend "$::toplevel mouseup [$::current_canvas canvasx $x] [$::current_canvas canvasy $y] 1"
+}
+
 ###########################################################
 # overwritten
-
-# I overwrite this just to be able to reset the cycle in auto-completion
-proc ::pd_bindings::sendkey {window state key iso shift} {
-    switch -- $key {
-        "BackSpace" { set iso ""; set key 8 }
-        "Tab"       { set iso ""; set key 9 }
-        "Return"    { set iso ""; set key 10 }
-        "Escape"    { set iso ""; set key 27 }
-        "Space"     { set iso ""; set key 32 }
-        "Delete"    { set iso ""; set key 127 }
-        "KP_Delete" { set iso ""; set key 127 }
-    }
-    if {$iso ne ""} {
-        scan $iso %c key
-    }
-    if {! [winfo exists $window]} {return}
-    set mytoplevel [winfo toplevel $window]
-    if {[winfo class $mytoplevel] eq "PatchWindow"} {
-        pdsend "$mytoplevel key $state $key $shift"
-        # auto-completion
-        # something was typed in so reset $::cycle
-        if {$key != 9} {set ::cycle false}
-    }
-}
 
 # change the text in an existing text box
 proc pdtk_text_set {tkcanvas tag text} {
     $tkcanvas itemconfig $tag -text $text
     # auto-completion: store typed text
-    set ::current_text $text
+    if {![winfo exists .pop]} {
+        if {[llength $::current_text] < 4} {
+	    # there are default whitespaces in an empty object
+            set ::current_text [string trimright $text " "]
+        } { set ::current_text $text }
+    }
 }
 
-# thanks Hans-Christoph Steiner
 proc pdtk_text_editing {mytoplevel tag editing} {
+    set ::toplevel $mytoplevel
     set tkcanvas [tkcanvas_name $mytoplevel]
     set rectcoords [$tkcanvas bbox $tag]
     if {$rectcoords ne ""} {
-        set ::editx [expr int([lindex $rectcoords 0])]
-        set ::edity [expr int([lindex $rectcoords 3])]
+        set ::editx  [expr {int([lindex $rectcoords 0])}]
+        set ::edity  [expr {int([lindex $rectcoords 3])}]
     }
     if {$editing == 0} {
         selection clear $tkcanvas
         # auto-completion
         set ::completions {}
-        set ::new_object false
         set ::lock_motion false
-        set ::cycle false
+        set ::canvas_bound 0
+        catch { destroy .pop }
     } {
         set ::editingtext($mytoplevel) $editing
         # auto-completion
-        set ::new_object $editing
         set ::current_canvas $tkcanvas
-        set ::current_tag $tag
+        if {$tag ne ""} {
+            set ::current_tag $tag
+        }
     }
+    set ::new_object $editing
     $tkcanvas focus $tag
+    if {[llength ::completions] > 0} {
+        bind $tkcanvas <KeyPress> {::completion::text_keys %K}
+    }
 }
 
 proc ::dialog_font::ok {gfxstub} {
@@ -254,12 +386,44 @@ proc ::dialog_font::ok {gfxstub} {
     apply $gfxstub $fontsize
     cancel $gfxstub
     # auto-completion
-    set $::font_size $fontsize
+    set ::font_size [expr {$fontsize - 1}];# uh ?
 }
+
+############################################################
+# utils
+
+# `prefix' from Bruce Hartweg <http://wiki.tcl.tk/44>
+proc ::completion::prefix {s1 s2} {
+    regexp {^(.*).*\0\1} "$s1\0$s2" all pref
+    return $pref
+}
+
+proc ::completion::try_common_prefix {} {
+    set found 0
+    set prefix [::completion::common_prefix]
+    if {$prefix ne $::current_text} {
+        ::completion::replace_text $prefix
+        focus .pop.f.lb ;# prevent errors in pdtk_text_editing
+        set ::current_text $prefix
+        set found 1
+    }
+    return $found
+}
+
+proc ::completion::common_prefix {} {
+    set prefix ""
+    if {[llength $::completions] > 1} {
+        set prefix [::completion::prefix \
+                        [lindex $::completions 0] \
+                        [lindex $::completions end]]
+    }
+    return $prefix
+}
+
 
 ###########################################################
 # main
 
 ::completion::init
 
-pdtk_post "loaded: autocompletion-plugin 0.33\n"
+pdtk_post "loaded: autocompletion-plugin 0.40\n"
